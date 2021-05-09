731dadbacd92e2b84b948f09f8bec12965a4e07d
ChenPaulYu <beasoftwarengineering@gmail.com>
2021-05-09 14:44:06 +0800

    redirect

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
-    $('#web').load('https://www.surveycake.com/s/WbXkZ')
-});
+
+    urls = [
+        'https://www.surveycake.com/s/WbXkZ', 
+        'https://www.surveycake.com/s/YLmOV', 
+        'https://www.surveycake.com/s/9vQPz',
+        'https://www.surveycake.com/s/OK4pZ', 
+        'https://www.surveycake.com/s/bWxVK', 
+        'https://www.surveycake.com/s/4lba0'
+    ]
+
+    index = Math.floor(Math.random() * urls.length)
+    window.location.href = urls[index]
+
+})
