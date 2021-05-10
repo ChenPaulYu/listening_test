884febe84ff0bd26342c69a769a75292250f2ce7
ChenPaulYu <beasoftwarengineering@gmail.com>
2021-05-10 17:34:37 +0800

    control one

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
 
     urls = [
-        'https://www.surveycake.com/s/WbXkZ',
-        // 'https://www.surveycake.com/s/YLmOV',
-        // 'https://www.surveycake.com/s/9vQPz',
+        // 'https://www.surveycake.com/s/WbXkZ',
+        'https://www.surveycake.com/s/YLmOV',
+        'https://www.surveycake.com/s/9vQPz',
         'https://www.surveycake.com/s/OK4pZ', 
-        // 'https://www.surveycake.com/s/bWxVK', 
-        // 'https://www.surveycake.com/s/4lba0', 
+        'https://www.surveycake.com/s/OK4pZ',
+        'https://www.surveycake.com/s/OK4pZ',
+        'https://www.surveycake.com/s/bWxVK', 
+        'https://www.surveycake.com/s/4lba0', 
+        'https://www.surveycake.com/s/4lba0',
+        'https://www.surveycake.com/s/4lba0',
     ]
 
     index = Math.floor(Math.random() * urls.length)
