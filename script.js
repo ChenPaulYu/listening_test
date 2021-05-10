921eae74fd38082917baf8b91a3de09f14242c81
ChenPaulYu <beasoftwarengineering@gmail.com>
2021-05-10 12:40:25 +0800

    change porb

--- script.js
+++ script.js
@@ -6,13 +6,15 @@ $(document).ready(function () {
         'https://www.surveycake.com/s/9vQPz',
         'https://www.surveycake.com/s/OK4pZ', 
         'https://www.surveycake.com/s/bWxVK', 
-        'https://www.surveycake.com/s/4lba0'
+        'https://www.surveycake.com/s/4lba0', 
+        'https://www.surveycake.com/s/4lba0',
+        'https://www.surveycake.com/s/4lba0',
+        'https://www.surveycake.com/s/4lba0',
+        'https://www.surveycake.com/s/4lba0',
     ]
 
     index = Math.floor(Math.random() * urls.length)
 
     $('#web').attr('src', urls[index])
-
-    console.log('load')
 
 })
