92f0aee321282a0f52f0ae34d7e8566df9a4722c
ChenPaulYu <beasoftwarengineering@gmail.com>
2021-05-10 13:32:34 +0800

    control 3, 6

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ $(document).ready(function () {
         'https://www.surveycake.com/s/WbXkZ',
         'https://www.surveycake.com/s/YLmOV', 
         'https://www.surveycake.com/s/YLmOV',
-        'https://www.surveycake.com/s/9vQPz',
+        // 'https://www.surveycake.com/s/9vQPz',
         'https://www.surveycake.com/s/OK4pZ', 
         'https://www.surveycake.com/s/bWxVK', 
-        'https://www.surveycake.com/s/4lba0', 
+        // 'https://www.surveycake.com/s/4lba0', 
     ]
 
     index = Math.floor(Math.random() * urls.length)
