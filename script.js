cf1acdafdde24056f83eea82ffbb39e6908f46f2
ChenPaulYu <beasoftwarengineering@gmail.com>
2021-05-11 16:22:53 +0800

    balance

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 $(document).ready(function () {
 
     urls = [
-        // 'https://www.surveycake.com/s/WbXkZ',
+        'https://www.surveycake.com/s/WbXkZ',
         'https://www.surveycake.com/s/YLmOV',
         'https://www.surveycake.com/s/9vQPz',
-        'https://www.surveycake.com/s/OK4pZ', 
-        'https://www.surveycake.com/s/OK4pZ',
         'https://www.surveycake.com/s/OK4pZ',
         'https://www.surveycake.com/s/bWxVK', 
-        'https://www.surveycake.com/s/4lba0', 
-        'https://www.surveycake.com/s/4lba0',
         'https://www.surveycake.com/s/4lba0',
     ]
 
