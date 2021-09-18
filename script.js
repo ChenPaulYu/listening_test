d24ce3e9fb9f47f597b850b38f6a70ecd24c18ed
ChenPaulYu <bernie40916@gmail.com>
2021-09-19 00:30:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
 
     urls  = [
-        'https://www.surveycake.com/s/69NLy',
-        'https://www.surveycake.com/s/ZmNdq',
-        'https://www.surveycake.com/s/M7kNB',
-        'https://www.surveycake.com/s/loBX3',
+//         'https://www.surveycake.com/s/69NLy',
+//         'https://www.surveycake.com/s/ZmNdq',
+//         'https://www.surveycake.com/s/M7kNB',
+//         'https://www.surveycake.com/s/loBX3',
         'https://www.surveycake.com/s/6WXOW', 
-        'https://www.surveycake.com/s/nwPWa',
+//         'https://www.surveycake.com/s/nwPWa',
 //         'https://www.surveycake.com/s/amPoN',
 //         'https://www.surveycake.com/s/BQRXg',
     ]
