2b379b3408983b760b2b9f62300a6211de79028c
ChenPaulYu <bernie40916@gmail.com>
2021-09-18 14:10:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
 
     urls  = [
-//         'https://www.surveycake.com/s/69NLy',
+        'https://www.surveycake.com/s/69NLy',
         'https://www.surveycake.com/s/ZmNdq',
+        'https://www.surveycake.com/s/M7kNB',
+        'https://www.surveycake.com/s/M7kNB',
         'https://www.surveycake.com/s/M7kNB',
         'https://www.surveycake.com/s/loBX3',
         'https://www.surveycake.com/s/6WXOW', 
